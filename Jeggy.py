a8ef58a087463f282863e4d3b0305fdd57c77c29
AmunCode <gjose055@fiu.edu>
2020-10-15 16:00:44 -0400

    Added code to acquired the URL of distinct auction url pages.

--- Jeggy.py
+++ Jeggy.py
@@ -17,3 +17,17 @@ Pages = list(dict.fromkeys(pages)) #remove duplicates from pages that will be sc
 for page in pages:
     addOn = str(page)
     print('https://bstocksupply.com/cell-phones?p='+addOn)
+
+URLs = []
+for page in Pages:
+    addOn = str(page)
+    page_res = requests.get('https://bstocksupply.com/cell-phones?p=' + addOn)
+    page_bs = bs4.BeautifulSoup(page_res.text, features='lxml')
+
+    # scrapes the current page for all auctions listed on the page
+    for link in page_bs.find_all('a'):
+        if link.has_attr('href'):
+            if 'auctions' in link.attrs['href']:
+                #print(link.attrs['href'])  # tracer line --remove from live code
+                URLs.append(link.attrs['href'])
+URLs = list(dict.fromkeys(URLs))
