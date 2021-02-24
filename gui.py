5c88aa8ac23e61ca4f63957d3a33fe4a5be1389a
AmunCode <gjose055@fiu.edu>
2021-02-24 15:52:47 -0500

    expanded UI with menu options

--- gui.py
+++ gui.py
@@ -10,15 +10,28 @@ class Window:
         self.menu = tk.Menu(self.window)
         self.window.config(menu=self.menu)
         self.window.geometry("700x500")
+        self.window.config(bg='light blue')
+        background_img = tk.PhotoImage("images/JEGgo.png")
+
+        # background_lable.grid(row=0, column=0)
         # Menus
         self.auctions_menu = tk.Menu(self.menu, tearoff=0)
         self.commHub_menu = tk.Menu(self.menu, tearoff=0)
         self.help_menu = tk.Menu(self.menu, tearoff=0)
+        self.scrape_menu = tk.Menu(self.auctions_menu, tearoff=0)
         # cascade set for each menu
         self.menu.add_cascade(label='Auctions', menu=self.auctions_menu)
         self.menu.add_cascade(label='CommerceHUB', menu=self.commHub_menu)
         self.menu.add_cascade(label='Help', menu=self.help_menu)
+
         # options under the Auctions menu option
-        self.auctions_menu.add_command(label="New Scrape")
+        self.auctions_menu.add_command(label="Scrape All")
+        self.auctions_menu.add_cascade(label="Scrape", menu=self.scrape_menu)
+
+        self.scrape_menu.add_command(label="Superior Auctions")
+        self.scrape_menu.add_command(label="Select Mobile Auctions")
+        self.scrape_menu.add_command(label="B-Stock Supply Auctions")
+
         self.auctions_menu.add_command(label="Open")
+
         self.window.mainloop()
